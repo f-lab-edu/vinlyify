bce0b2c99987bca885a57a7c8101fa00725bcf5c
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-05-27 02:59:10 +0900

    feat: call handleSearchKeyword on enter press

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,19 +1,34 @@
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 export default function SearchBar({
   searchWord,
+  defaultSearchWord,
   setSearchWord,
   handleSearchKeyword,
 }: {
   searchWord: string;
+  defaultSearchWord: string;
   setSearchWord: Dispatch<SetStateAction<string>>;
   handleSearchKeyword: () => void;
 }) {
+  const [useDefaultKeySearch, setUseDefaultKeySearch] = useState(true);
+
+  const handleUseDefaultKeySearch = useCallback(() => {
+    if (useDefaultKeySearch) setUseDefaultKeySearch(false);
+  }, [useDefaultKeySearch]);
+
   return (
     <>
-      <Input value={searchWord} onChange={e => setSearchWord(e.target.value)} />
+      <Input
+        useDefaultValue={useDefaultKeySearch}
+        defaultSearchWord={defaultSearchWord}
+        value={searchWord}
+        onChange={e => setSearchWord(e.target.value)}
+        onKeyUp={e => e.key === 'Enter' && handleSearchKeyword()}
+        onKeyDown={handleUseDefaultKeySearch}
+      />
       <Button outline onClick={handleSearchKeyword}>
         검색
       </Button>
