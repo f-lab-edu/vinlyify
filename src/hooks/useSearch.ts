d79a6b1ebeeef6a2e93fd7c9c31591585654a096
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-05-27 11:29:50 +0900

    refactor: separate search result to custom hook

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,88 @@
+import { searchFromMyTopOne, searchKeyword } from '@/api/spotify';
+import { SearchResult } from '@/models/searchResult';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+export function useSearch() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchWord, setSearchWord] = useState<string>(
+    searchParams.get('keyword') ?? '',
+  );
+  const [searchResult, setSearchResult] = useState<SearchResult | null>(null);
+  const [defaultSearchWord, setDefaultSearchWord] = useState<string>('');
+
+  const [useDefaultKeySearch, setUseDefaultKeySearch] = useState(true);
+
+  const handleUseDefaultKeySearch = useCallback(() => {
+    if (useDefaultKeySearch) setUseDefaultKeySearch(false);
+  }, [useDefaultKeySearch]);
+
+  /**
+   * 디폴트 검색 기록으로 설정
+   */
+  const getDefaultSearch = () => {
+    searchFromMyTopOne().then(v => {
+      const { keyword, response } = v;
+      setDefaultSearchWord(keyword);
+      setSearchResult(response);
+    });
+  };
+
+  useEffect(() => {
+    if (
+      searchWord === '' &&
+      defaultSearchWord === '' &&
+      !searchParams.get('keyword')
+    ) {
+      getDefaultSearch();
+    }
+  }, [
+    searchParams,
+    defaultSearchWord,
+    searchWord,
+    searchResult,
+    setDefaultSearchWord,
+    setSearchResult,
+  ]);
+
+  /**
+   * 검색어를 url param으로 추가하기
+   * */
+  const handleSearchKeyword = useCallback(() => {
+    searchKeyword(searchWord).then(v => setSearchResult(v));
+    setSearchParams([
+      ...[...searchParams].filter(v => v[0] !== 'keyword'),
+      ['keyword', searchWord],
+    ]);
+  }, [searchWord, searchParams, setSearchParams, setSearchResult]);
+
+  /**
+   * 새로고침, 뒤로 가기, 검색창에 직접 입력 시에 url param에 검색어 정보를 가져오기
+   */
+  useMemo(() => {
+    if (searchParams.get('keyword')) {
+      setSearchWord(`${searchParams.get('keyword')}`);
+      searchKeyword(`${searchParams.get('keyword')}`).then(v =>
+        setSearchResult(v),
+      );
+    } else {
+      // '/search'로 뒤로 가기했을 경우 검색어 제거
+      setSearchWord('');
+      getDefaultSearch();
+    }
+  }, [searchParams, setSearchResult, setSearchWord]);
+
+  return {
+    searchParams,
+    handleSearchKeyword,
+    setSearchParams,
+    searchWord,
+    setSearchWord,
+    useDefaultKeySearch,
+    handleUseDefaultKeySearch,
+    defaultSearchWord,
+    setDefaultSearchWord,
+    searchResult,
+    setSearchResult,
+  };
+}
