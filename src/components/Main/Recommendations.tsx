1f48f8026b11571c31e9c9c725b1d14608860461
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-05-29 22:06:42 +0900

    refactor: separate recommedation component and toptrack component

--- src/components/Main/Recommendations.tsx
+++ src/components/Main/Recommendations.tsx
@@ -1,7 +1,11 @@
-export default function Recommendations({
-  recommendations,
-}: {
-  recommendations: Recommendations | null;
-}) {
-  return <>{JSON.stringify(recommendations)}</>;
+import { useRecommendations } from '@/query';
+
+export default function Recommendations() {
+  const { data } = useRecommendations();
+
+  return (
+    <>
+      <h1>recommendations</h1> {JSON.stringify(data)}
+    </>
+  );
 }
--- src/components/Main/TopTrack.tsx
+++ src/components/Main/TopTrack.tsx
@@ -1,3 +1,12 @@
-export default function TopTrack({ topTracks }: { topTracks: Tracks | null }) {
-  return <>{JSON.stringify(topTracks)}</>;
+import { useTopTracks } from '@/query';
+
+export default function TopTrack() {
+  const { data } = useTopTracks();
+
+  return (
+    <>
+      <h1>top tracks</h1>
+      {JSON.stringify(data)}
+    </>
+  );
 }
