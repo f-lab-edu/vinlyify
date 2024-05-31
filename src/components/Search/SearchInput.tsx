d94dc48ade64b43089342d2d5379a6a14f86bc29
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-05-31 19:18:57 +0900

    feat: set keyword url param as highest priority,set param as debounced keyword

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -1,16 +1,24 @@
+import Container from '@/components/_shared/Container';
+import Input from '@/components/_shared/Input';
+import { URL_PARAMS } from '@/constants';
 import { useSearchKeyword } from '@/hooks/query/useSearchKeyword';
 import { useDebounce } from '@/hooks/useDebounce';
 import {
   ChangeEventHandler,
   KeyboardEventHandler,
-  MouseEventHandler,
   useMemo,
   useState,
 } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export default function SearchInput() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [keyword, setKeyword] = useState('');
-  const { refetch, data } = useSearchKeyword(keyword);
+  const { refetch, data } = useSearchKeyword(
+    searchParams.has(URL_PARAMS.KEYWORD)
+      ? searchParams.get(URL_PARAMS.KEYWORD)
+      : keyword,
+  );
 
   const placeHolder = useMemo(() => {
     const defaultSearchWord = data?.albums?.href.match(
@@ -21,9 +29,10 @@ export default function SearchInput() {
         ? `${defaultSearchWord[0].replaceAll('+', ' ')}`
         : '';
     return decodeURI(dsw);
-  }, [data?.albums?.href]);
+  }, [data]);
 
   const debouncedRequest = useDebounce(() => {
+    if (keyword) setSearchParams({ keyword });
     refetch();
   });
 
@@ -37,20 +46,14 @@ export default function SearchInput() {
     if (e.key === 'Enter') debouncedRequest();
   };
 
-  const onClick: MouseEventHandler<HTMLButtonElement> = e => {
-    e.preventDefault();
-    debouncedRequest();
-  };
-
   return (
-    <>
-      <input
-        value={keyword}
-        placeholder={placeHolder}
+    <Container>
+      <Input
+        value={keyword ?? placeHolder}
+        placeHolder={placeHolder}
         onChange={onChange}
         onKeyUp={onKeyUp}
       />
-      <button onClick={onClick}>검색</button>
-    </>
+    </Container>
   );
 }
