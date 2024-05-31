2d2d087966f006e1c75c57c8d815009fd5ef8900
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-05-31 19:14:36 +0900

    feat: add search keyword to url param

--- src/hooks/query/useSearchKeyword.ts
+++ src/hooks/query/useSearchKeyword.ts
@@ -1,19 +1,33 @@
 import { searchFromMyTopOne, searchKeyword } from '@/api/spotify';
-import { useQuery } from '@tanstack/react-query';
+import { URL_PARAMS } from '@/constants';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 
 export const useSearchKeyword = (keyword: string | null) => {
+  const [searchParams] = useSearchParams();
+
   // 검색어를 입력하지 않은 경우 내 top1 아티스트 기준으로
-  const { data } = useQuery({
+  const defaultSearch = useQuery({
     queryKey: useSearchKeyword.queryKey(),
     queryFn: () => searchFromMyTopOne().then(v => v.response),
+    staleTime: Infinity,
   });
 
+  const keywordParam = searchParams.get(URL_PARAMS.KEYWORD);
+
   const res = useQuery({
-    queryKey: useSearchKeyword.queryKey(),
-    queryFn: () => searchKeyword(keyword),
-    enabled: false,
-    initialData: data,
+    queryKey: useSearchKeyword.queryKey(keywordParam),
+    queryFn: () => searchKeyword(keywordParam),
+    notifyOnChangeProps: ['data'],
+    staleTime: 2_000,
+    placeholderData: keepPreviousData,
   });
+  if (!searchParams.has(URL_PARAMS.KEYWORD) && !keyword) return defaultSearch;
+
   return res;
 };
-useSearchKeyword.queryKey = () => ['search', 'list'];
+useSearchKeyword.queryKey = (keyword?: string | null) => {
+  const qk = ['search', 'list'];
+  if (keyword == null || keyword === '') return qk;
+  return [...qk, keyword];
+};
