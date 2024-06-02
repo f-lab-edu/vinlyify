1ea1714cc122a213abcb3187ea154cab51af61c6
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-02 15:43:39 +0900

    feat: style vinyl body  component

---
+++ src/components/MusicInfo/VinylBody.tsx
@@ -0,0 +1,20 @@
+import '@/style/music-info/vinyl-body.scss';
+import classNames from 'classnames';
+import { FC, ReactNode } from 'react';
+import { ImgUrlProps } from './Cover';
+
+interface ContainerProps extends ImgUrlProps {
+  children: ReactNode;
+}
+
+const VinylBody: FC<ContainerProps> = ({ children, imgUrl }) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${imgUrl})` }}
+      className={classNames('VinylBody')}
+    >
+      {children}
+    </div>
+  );
+};
+export default VinylBody;
