a9d5f483a5e49529b164c8368324efb291c78d18
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-05 15:19:48 +0900

    feat: update placeholder image link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -25,7 +25,6 @@ export const DEFAULT_PLAY_TRACK = 'spotify:album:5ht7ItJgpBH7W6vJ5BqpPr';
 
 export const SPOTIFY_WEB_API = new SpotifyWebApi();
 
-export const PLACEHOLDER_IMAGE =
-  'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg';
+export const PLACEHOLDER_IMAGE = 'https://placehold.co/600x600?text=NO IMAGE';
 
 export const ACCESS_TOKEN = 'access_token';
