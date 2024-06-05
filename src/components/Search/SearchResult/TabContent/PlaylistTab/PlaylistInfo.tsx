450691ea85bad7256461533895a52d1da742ffe0
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-05 19:08:32 +0900

    feat: create playlist component

---
+++ src/components/Search/SearchResult/TabContent/PlaylistTab/PlaylistInfo.tsx
@@ -0,0 +1,45 @@
+import { playTrack } from '@/api/spotify';
+import { PLACEHOLDER_IMAGE } from '@/constants';
+import { Playlist } from '@/models/Playlist';
+import Card from '../_shared/Card';
+import Logo from '../_shared/Logo';
+import PlayButton from '../_shared/PlayButton';
+import ProfileImage from '../_shared/ProfileImage';
+import './playlist-info.scss';
+
+const PlaylistInfo = ({ item }: { item: Playlist }) => {
+  const onPlayCurrentAlbum = () => playTrack({ context_uris: item?.uri });
+
+  function createMarkup(text: string) {
+    return { __html: text };
+  }
+
+  return (
+    <Card
+      title={item?.name}
+      title_tag={`by. ${item?.owner?.display_name}`}
+      left={
+        <ProfileImage
+          imgUrl={
+            item?.images === undefined
+              ? PLACEHOLDER_IMAGE
+              : item?.images[0]?.url
+          }
+          link={<Logo url={item?.external_urls?.spotify ?? ''} />}
+        />
+      }
+      playButton={<PlayButton onPlayCurrent={onPlayCurrentAlbum} />}
+    >
+      <li>total tracks : {item?.tracks?.total}</li>
+
+      <li>
+        <div
+          dangerouslySetInnerHTML={createMarkup(item.description)}
+          className="playlist-info"
+        ></div>
+      </li>
+    </Card>
+  );
+};
+
+export default PlaylistInfo;
--- src/components/Search/SearchResult/TabContent/PlaylistTab/index.tsx
+++ src/components/Search/SearchResult/TabContent/PlaylistTab/index.tsx
@@ -1,49 +1,14 @@
+import { Loading } from '@/components/Main';
 import { Playlist } from '@/models/Playlist';
+import Grid from '../_shared/Grid';
+import PlaylistInfo from './PlaylistInfo';
 
 const PlaylistsTab = ({ tabItem }: { tabItem: Playlist[] }) => {
+  if (tabItem?.length === 0) return <Loading />;
   return (
-    <>
-      {/* <FlexWrap orientation={'cl'} full={false}>
-        <ProfileImage src={tabItem?.images[0]?.url} sz="12rem" round={false} />
-        <PlayButton onClick={() => playTrack({ context_uris: tabItem?.uri })}>
-          play current playlist
-        </PlayButton>
-      </FlexWrap> */}
-
-      {/* <ul>
-        <h1>{tabItem?.name}</h1>
-        <li>collaborative : {`${tabItem?.collaborative}`}</li>
-        <li>description : {tabItem?.description || 'nothing to show'}</li>
-        <Link to={tabItem?.external_urls?.spotify}>external link</Link>
-        <Link to={tabItem?.href}>link to playlist</Link>
-        <>playlist id: {tabItem?.id}</>
-
-        <ul>
-          playlist owner:
-          <>name: {tabItem?.owner?.display_name}</>
-          <li>
-            external_urls:{' '}
-            <Link to={tabItem?.external_urls?.spotify}>external link</Link>
-          </li>
-          <li>owner id: {tabItem?.owner?.id}</li>
-          <li>owner type: {tabItem?.owner?.type}</li>
-          <li>owner uri: {tabItem?.owner?.uri}</li>
-        </ul>
-        {tabItem?.primary?.color && (
-          <li>primary color: {tabItem?.primary?.color}</li>
-        )}
-        {tabItem?.public && <div>public: {`${tabItem?.public}`}</div>}
-
-        <li>snapshot id: {tabItem?.snapshot_id}</li>
-        <li>
-          tracks: <Link to={tabItem?.tracks?.href}>playlist link</Link> total
-          tracks: {tabItem?.tracks?.total}
-        </li>
-
-        <li>type: {tabItem?.type}</li>
-      </ul> */}
-      {JSON.stringify(tabItem)}
-    </>
+    <Grid>
+      {tabItem?.map(item => <PlaylistInfo item={item} key={item.id} />)}
+    </Grid>
   );
 };
 
