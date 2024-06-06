676e14fd3030e8c5bd4be64865c85fb0e04e14ca
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-06 15:25:30 +0900

    refactor: use multiProfile hooks instead

--- src/components/Search/SearchResult/TabContent/AlbumTab/AlbumItem.tsx
+++ src/components/Search/SearchResult/TabContent/AlbumTab/AlbumItem.tsx
@@ -1,13 +1,13 @@
 import { playTrack } from '@/api/spotify';
 import { PLACEHOLDER_IMAGE } from '@/constants';
 import { Album } from '@/models/Album';
-import { ExternalUrls } from '@/models/MetaInfo';
 import { useMemo } from 'react';
 import Card from '../_shared/Card';
 import Logo from '../_shared/Logo';
 import MultiProfile from '../_shared/MultiProfile';
 import PlayButton from '../_shared/PlayButton';
 import ProfileImage from '../_shared/ProfileImage';
+import { useMultiProfileImg } from '../_shared/hooks/useMultiProfileImg';
 
 const AlbumItem = ({
   item,
@@ -24,30 +24,17 @@ const AlbumItem = ({
     return item?.images[0]?.url;
   }, [item]);
 
-  const artistInfo = useMemo(() => {
-    return item?.artists?.map(v => {
-      const spotifyLink = (v.external_urls?.spotify
-        ? v.external_urls?.spotify
-        : '/') as unknown as ExternalUrls;
-      if (artistImgUrls) {
-        return { img: artistImgUrls.get(v.id) as string, link: spotifyLink };
-      } else {
-        return { img: PLACEHOLDER_IMAGE, link: spotifyLink };
-      }
-    });
-  }, [item?.artists, artistImgUrls]);
+  const artistInfo = useMultiProfileImg({ item, artistImgUrls });
 
   return (
     <Card
       title={item?.name}
       title_tag={[item?.album_type, item?.release_date].join(' ')}
       left={
-        <>
-          <ProfileImage
-            imgUrl={validImageUrl}
-            link={<Logo url={item?.external_urls?.spotify || ''} />}
-          />
-        </>
+        <ProfileImage
+          imgUrl={validImageUrl}
+          link={<Logo url={item?.external_urls?.spotify || ''} />}
+        />
       }
       playButton={
         item.is_playable ? (
--- src/components/Search/SearchResult/TabContent/AlbumTab/index.tsx
+++ src/components/Search/SearchResult/TabContent/AlbumTab/index.tsx
@@ -1,8 +1,6 @@
 // import Paging from '../_shared/Paging';
-import { getArtists } from '@/api/spotify';
-import { PLACEHOLDER_IMAGE } from '@/constants';
 import { Album } from '@/models/Album';
-import { useEffect, useState } from 'react';
+import { useMultiProfileMap } from '../_shared/hooks/useMultiProfileMap';
 import AlbumList from './AlbumList';
 
 const AlbumTab = ({
@@ -12,28 +10,9 @@ const AlbumTab = ({
   tabItem: Album[];
   // pagingInfo: Pagination;
 }) => {
-  const [artistImgs, setArtistImgs] = useState<Map<string, string>>(new Map());
-
-  useEffect(() => {
-    if (tabItem.length > 0) {
-      getArtists([
-        ...new Set(
-          tabItem?.map(item => item?.artists?.map(artist => artist.id)).flat(1),
-        ),
-      ]).then(v => {
-        const res = v.reduce((acc, curr) => {
-          if (curr['images']) {
-            return acc.set(`${curr['id']}`, curr['images'][0].url);
-          }
-          return acc.set(`${curr['id']}`, PLACEHOLDER_IMAGE);
-        }, new Map()) as Map<string, string>;
-        setArtistImgs(res);
-      });
-    }
-  }, [tabItem]);
+  const artistImgs = useMultiProfileMap({ tabItem });
   return (
     <>
-      {/* {JSON.stringify(pagingInfo)} */}
       {/* <Paging pagingInfo={pagingInfo} /> */}
       <AlbumList tabItem={tabItem} artistImgUrls={artistImgs} />
     </>
