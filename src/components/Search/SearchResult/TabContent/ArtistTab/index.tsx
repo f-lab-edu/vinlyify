64a927db23e18f49aa88865b7148e4941873533b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-06 15:27:08 +0900

    chore: replace with renamed files

--- src/components/Search/SearchResult/TabContent/ArtistTab/index.tsx
+++ src/components/Search/SearchResult/TabContent/ArtistTab/index.tsx
@@ -1,13 +1,13 @@
 import { Loading } from '@/components/Main';
 import { Artist } from '@/models/Profile';
 import Grid from '../_shared/Grid';
-import ArtistInfo from './ArtistInfo';
+import ArtistItem from './ArtistItem';
 
 const ArtistTab = ({ tabItem }: { tabItem: Artist[] }) => {
   if (tabItem?.length === 0) return <Loading />;
   return (
     <Grid>
-      {tabItem?.map(item => <ArtistInfo item={item} key={item.id} />)}
+      {tabItem?.map(item => <ArtistItem item={item} key={item.id} />)}
     </Grid>
   );
 };
--- src/components/Search/SearchResult/TabContent/PlaylistTab/index.tsx
+++ src/components/Search/SearchResult/TabContent/PlaylistTab/index.tsx
@@ -1,13 +1,13 @@
 import { Loading } from '@/components/Main';
 import { Playlist } from '@/models/Playlist';
 import Grid from '../_shared/Grid';
-import PlaylistInfo from './PlaylistInfo';
+import PlaylistItem from './PlaylistItem';
 
 const PlaylistsTab = ({ tabItem }: { tabItem: Playlist[] }) => {
   if (tabItem?.length === 0) return <Loading />;
   return (
     <Grid>
-      {tabItem?.map(item => <PlaylistInfo item={item} key={item.id} />)}
+      {tabItem?.map(item => <PlaylistItem item={item} key={item.id} />)}
     </Grid>
   );
 };
