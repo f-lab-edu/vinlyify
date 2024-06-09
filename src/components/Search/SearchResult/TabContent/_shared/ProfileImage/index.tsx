54059fcd4e79e8841a6da963850dff7735cc4374
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-09 20:17:42 +0900

    refactor: update component typing to props destructuring

--- src/components/Search/SearchResult/TabContent/_shared/ProfileImage/index.tsx
+++ src/components/Search/SearchResult/TabContent/_shared/ProfileImage/index.tsx
@@ -1,17 +1,17 @@
 import { PLACEHOLDER_IMAGE } from '@/constants';
 import classNames from 'classnames';
-import { FC, ReactNode, SVGProps } from 'react';
+import { ReactNode, SVGProps } from 'react';
 import './profile-image.scss';
 export interface ImgUrlProps extends SVGProps<SVGImageElement> {
   imgUrl?: string;
   link: ReactNode;
 }
 
-const CoverImage: FC<ImgUrlProps> = ({ imgUrl, link }) => {
+const CoverImage = ({ imgUrl, link }: ImgUrlProps) => {
   return (
     <>
       <img
-        src={imgUrl === undefined ? PLACEHOLDER_IMAGE : imgUrl}
+        src={imgUrl == null ? PLACEHOLDER_IMAGE : imgUrl}
         alt={imgUrl}
         loading="lazy"
         className={classNames('cover-image')}
