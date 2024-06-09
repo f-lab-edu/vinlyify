95cdce9e36fcb27d83e75472ff7e3a9327256cbf
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-09 21:41:50 +0900

    chore: rename classname

--- src/components/Search/SearchResult/TabContent/_shared/CoverImage/CoverImageSkeleton.tsx
+++ src/components/Search/SearchResult/TabContent/_shared/CoverImage/CoverImageSkeleton.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import '../skeleton.scss';
 
 const CoverImageSkeleton = () => {
-  return <span className={classNames('profileImage', 'skeleton')} />;
+  return <span className={classNames('cover-image', 'skeleton')} />;
 };
 
 export default CoverImageSkeleton;
