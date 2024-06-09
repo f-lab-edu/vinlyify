915bce70c258f2a51a0b15a322f007e539ebf9cf
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-09 19:03:44 +0900

    feat: create Profile image component

---
+++ src/components/Search/SearchResult/TabContent/_shared/ProfileImage/index.tsx
@@ -0,0 +1,25 @@
+import { PLACEHOLDER_IMAGE } from '@/constants';
+import classNames from 'classnames';
+import { FC, ReactNode, SVGProps } from 'react';
+import './profile-image.scss';
+export interface ImgUrlProps extends SVGProps<SVGImageElement> {
+  imgUrl?: string;
+  round?: boolean;
+  link: ReactNode;
+}
+
+const ProfileImage: FC<ImgUrlProps> = ({ imgUrl, round, link }) => {
+  return (
+    <>
+      <img
+        src={imgUrl === undefined ? PLACEHOLDER_IMAGE : imgUrl}
+        alt={imgUrl}
+        loading="lazy"
+        className={classNames('profile-image', { round })}
+      />
+      {link}
+    </>
+  );
+};
+
+export default ProfileImage;
