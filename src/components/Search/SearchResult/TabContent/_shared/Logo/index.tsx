57ff7ee8c2f41d15e1f5c4487f659f51a70cdcde
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-09 20:48:51 +0900

    feat: add logo component

---
+++ src/components/Search/SearchResult/TabContent/_shared/Logo/index.tsx
@@ -0,0 +1,24 @@
+import Spotify from '@/assets/spotifyLogo.svg';
+import classNames from 'classnames';
+import { HtmlHTMLAttributes } from 'react';
+import { Link } from 'react-router-dom';
+import './logo.scss';
+
+export interface logoProps extends HtmlHTMLAttributes<HTMLDivElement> {
+  url: string;
+  fill?: string;
+}
+
+const Logo = ({ url, fill }: logoProps) => {
+  return (
+    <Link
+      to={url}
+      className={classNames(`logo${fill === 'skeleton' ? '-skeleton' : ''}`, {
+        fill,
+      })}
+    >
+      <Spotify />
+    </Link>
+  );
+};
+export default Logo;
