6b76d1c6ca183d9bc68a8fdf1694bc1b26ea4b0a
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-09 19:12:43 +0900

    chore: remove unused props

--- src/components/Search/SearchResult/TabContent/_shared/ProfileImage/index.tsx
+++ src/components/Search/SearchResult/TabContent/_shared/ProfileImage/index.tsx
@@ -4,18 +4,17 @@ import { FC, ReactNode, SVGProps } from 'react';
 import './profile-image.scss';
 export interface ImgUrlProps extends SVGProps<SVGImageElement> {
   imgUrl?: string;
-  round?: boolean;
   link: ReactNode;
 }
 
-const CoverImage: FC<ImgUrlProps> = ({ imgUrl, round, link }) => {
+const CoverImage: FC<ImgUrlProps> = ({ imgUrl, link }) => {
   return (
     <>
       <img
         src={imgUrl === undefined ? PLACEHOLDER_IMAGE : imgUrl}
         alt={imgUrl}
         loading="lazy"
-        className={classNames('cover-image', { round })}
+        className={classNames('cover-image')}
       />
       {link}
     </>
