57d94202c69edb1d2a9ae30a70d266495459325e
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-11 14:20:32 +0900

    chore: svg setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,25 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import type { UserConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import type { InlineConfig } from 'vitest';
 
 type ViteConfig = UserConfig & { test: InlineConfig };
 const config: ViteConfig = {
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
