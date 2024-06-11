c820d54f79fdfd4c941368c840cfc761a4d3580b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-11 11:46:26 +0900

    feat: split tab content and selection component

---
+++ src/components/Search/SearchResult/index.tsx
@@ -0,0 +1,11 @@
+import TabContent from './TabContent';
+import TabSelection from './TabSelection/';
+
+export default function SearchResult() {
+  return (
+    <>
+      <TabSelection />
+      <TabContent />
+    </>
+  );
+}
