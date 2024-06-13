20045c78132d3db7a1d446ccbd7eee6d449f6ab7
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-13 14:43:40 +0900

    feat: replace profile scss to module

--- src/components/Search/SearchResult/TabContent/_shared/MutliProfile/Profile.tsx
+++ src/components/Search/SearchResult/TabContent/_shared/MutliProfile/Profile.tsx
@@ -1,8 +1,11 @@
 import { ExternalUrls } from '@/models/MetaInfo';
 import { Artist } from '@/models/Profile';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import Image from '../Image';
+import Style from './profile.module.scss';
+
+const style = classNames.bind(Style);
 
 export interface ProfileInfo {
   img: string;
@@ -14,14 +17,14 @@ const Profile = ({ profile }: { profile: ProfileInfo }) => {
   const link = (profile?.link ?? '#') as unknown as ExternalUrls['spotify'];
 
   return (
-    <div className={classNames('profile')}>
+    <div className={style('profile')}>
       <Link
-        className={classNames('account-photo')}
+        className={style('account-photo')}
         to={link}
         aria-disabled={profile?.link == null}
       >
-        <div className="mask">
-          <Image url={profile.img} classNameArr={['photo']} />
+        <div className={style('mask')}>
+          <Image url={profile.img} classNameArr={[]} />
         </div>
       </Link>
     </div>
