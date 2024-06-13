df938c864e57144838006c3357f17f64d0902c46
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-13 15:49:21 +0900

    refactor: replace type assertion to generics

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -170,7 +170,5 @@ export async function searchFromMyTopOne() {
  * artist top tracks : 재생 중인 트랙의 아티스트의 top 10 tracks
  */
 export async function getArtistTopTracks({ id }: { id: string }) {
-  return api.get(`artists/${id}/top-tracks`, {}).json() as unknown as {
-    tracks: Track[];
-  };
+  return api.get(`artists/${id}/top-tracks`, {}).json<{ tracks: Track[] }>();
 }
