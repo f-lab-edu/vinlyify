147ec064e9ad451fd4ce371ebc717a87a9f44ed8
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-13 19:23:52 +0900

    feat: create header component

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,32 +1,11 @@
-import { API, PAGE } from '@/constants';
-import { useAuth } from '@/hooks/useAuth';
-
-import { Link, Outlet } from 'react-router-dom';
-
-const logIn = () => {
-  window.location.replace(API.LOGIN);
-};
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 export const BaseLayout = () => {
-  const { data, logOut } = useAuth();
-
   return (
     <>
-      <header>
-        <Link to={PAGE.MAIN}>home</Link>
-        {!data ? (
-          <button onClick={logIn}>login</button>
-        ) : (
-          <>
-            <Link to={PAGE.SEARCH}>search</Link>
-            <Link to={PAGE.MUSIC_INFO}>currently playing</Link>
-            <button onClick={logOut}> logout</button>
-          </>
-        )}
-      </header>
+      <Header />
       <Outlet />
-
-      <footer>footer</footer>
     </>
   );
 };
