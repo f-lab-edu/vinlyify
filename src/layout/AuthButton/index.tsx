178f77a000cb9e7015d1b0ea1a7c3190eebcd17c
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-14 14:00:59 +0900

    refactor: remove memo

--- src/layout/AuthButton/index.tsx
+++ src/layout/AuthButton/index.tsx
@@ -1,7 +1,6 @@
 import { API } from '@/constants';
 import { useAuth } from '@/hooks/useAuth';
 import classNames from 'classnames/bind';
-import { useMemo } from 'react';
 import Style from './auth-button.module.scss';
 
 const style = classNames.bind(Style);
@@ -13,20 +12,12 @@ const logIn = () => {
 const AuthButton = ({ action }: { action: 'LOGIN' | 'LOGOUT' }) => {
   const { logOut } = useAuth();
 
-  const AuthAction = useMemo(
-    () => ({
-      LOGIN: { tag: '로그인', action: logIn },
-      LOGOUT: { tag: '로그아웃', action: logOut },
-    }),
-    [logOut],
-  );
-
   return (
     <button
-      onClick={AuthAction[action].action}
+      onClick={action === 'LOGIN' ? logIn : logOut}
       className={style('button', 'auth-button')}
     >
-      {AuthAction[action].tag}
+      {action === 'LOGIN' ? '로그인' : '로그아웃'}
     </button>
   );
 };
