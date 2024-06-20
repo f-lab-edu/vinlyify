d7dea66cbe19910379a9270938efc2a2aea5c467
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-20 17:12:44 +0900

    feat: create album tab

--- src/components/Search/SearchResult/TabContent/AlbumTab/index.tsx
+++ src/components/Search/SearchResult/TabContent/AlbumTab/index.tsx
@@ -1,5 +1,7 @@
 import { Album } from '@/models/Album';
 import { Pagination } from '@/models/Pagination';
+import { useMultiProfileMap } from '../_shared/MutliProfile/hooks/useMultiProfileMap';
+import AlbumList from './AlbumList';
 
 const AlbumTab = ({
   tabItem,
@@ -8,10 +10,11 @@ const AlbumTab = ({
   tabItem: Album[];
   currentTabPagingInfo: Pagination | null;
 }) => {
+  const artistImgs = useMultiProfileMap({ tabItem });
   return (
     <>
+      <AlbumList tabItem={tabItem} artistImgUrls={artistImgs} />
       <div>{JSON.stringify(currentTabPagingInfo)}</div>
-      {JSON.stringify(tabItem)}
     </>
   );
 };
