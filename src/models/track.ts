af825bae56b17dd64302f0a8175fcf6df7a31a0e
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-20 19:08:50 +0900

    feat: add is_playable to Track type

--- src/models/track.ts
+++ src/models/track.ts
@@ -4,7 +4,7 @@ import { Pagination } from '@/models/Pagination';
 import { Artist } from '@/models/Profile';
 
 export interface Track extends MetaInfo {
-  album: Album[];
+  album: Album;
   artists: Artist[];
   available_markets: Pick<Album, 'available_markets'>;
   disc_number: number;
@@ -15,6 +15,7 @@ export interface Track extends MetaInfo {
   popularity: number;
   preview_url: string;
   track_number: number;
+  is_playable?: boolean;
 }
 
 export interface Tracks extends Pagination {
