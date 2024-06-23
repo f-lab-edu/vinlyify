c80d9c170764716fef3658d80f469b5895e90d30
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-23 13:11:26 +0900

    refactor: move followerCountFormatter to utils file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-import { Track } from '@/models/track';
-
 /**
  * 참고 : https://gist.github.com/ca0v/73a31f57b397606c9813472f7493a940
  * */
@@ -12,22 +10,4 @@ export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), waitFor);
   };
-}
-
-/**
- * 밀리초로 되어 있는 UTC 시간을 HH:MM:SS 형식으로 변환
- */
-export function UTC2HHMMSS({
-  duration_ms,
-}: {
-  duration_ms: Track['duration_ms'];
-}) {
-  const time = new Date(duration_ms);
-  const MMSS = [
-    `${time.getUTCMinutes()}`.padStart(2, '0'),
-    `${time.getUTCSeconds()}`.padStart(2, '0'),
-  ];
-  return time.getUTCHours() > 0
-    ? [`${time.getUTCHours()}`.padStart(2, '0'), ...MMSS].join(':')
-    : MMSS.join(':');
 }
