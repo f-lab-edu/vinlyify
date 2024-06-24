a2d75a9bad567e51ebcdb71661beb66c5729543a
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-24 10:52:15 +0900

    refactor: display none when no reccomendations exists

--- src/components/Main/Recommendations/index.tsx
+++ src/components/Main/Recommendations/index.tsx
@@ -5,13 +5,13 @@ import Table from './Table';
 export default function Recommendations() {
   const { data } = useRecommendations();
 
-  if (data?.tracks != null) {
+  if (data?.tracks == null) {
     return null;
   }
   return (
     <>
       <AnimatedTitle>Recommendations</AnimatedTitle>
-      <Table items={data?.tracks} />
+      <Table items={data.tracks} />
     </>
   );
 }
