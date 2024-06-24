985a14633fac795177abf7f492448a74749f78c3
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-24 12:18:30 +0900

    chore: relocate play/pause button

--- src/components/_shared/Button/PauseButton.tsx
+++ src/components/_shared/Button/PauseButton.tsx
@@ -1,16 +1,15 @@
 import { pauseTrack } from '@/api/spotify';
 import PauseIcon from '@/assets/pauseIcon.svg';
-import { useDebounce } from '@/hooks/useDebounce';
 import classNames from 'classnames/bind';
-import Style from '../button.module.scss';
+import Style from './button.module.scss';
 
 const style = classNames.bind(Style);
 
 const PauseButton = () => {
-  const onPauseDebounceHandler = useDebounce(() => pauseTrack({}));
+  const handlePause = () => pauseTrack({});
 
   return (
-    <button className={style('button')} onClick={onPauseDebounceHandler}>
+    <button className={style('button')} onClick={handlePause}>
       <PauseIcon />
     </button>
   );
