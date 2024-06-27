8d395f0ebc6e7db05cc7e3658610677accf80913
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-27 13:27:05 +0900

    fix: remove undefined type

--- src/components/Main/Recommendations/Table/index.tsx
+++ src/components/Main/Recommendations/Table/index.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 
 import PauseButton from '@/components/_shared/Button/PlayPauseButton/PauseButton';
 import PlayButton from '@/components/_shared/Button/PlayPauseButton/PlayButton';
+import { PLACEHOLDER_IMAGE } from '@/constants/image';
 import { useCurrentPlayingTrack } from '@/hooks/query/useCurrentPlayingTrack';
 import { TrackSearchResult } from '@/models/Spotify';
 import Style from './table.module.scss';
@@ -10,16 +11,23 @@ const style = classNames.bind(Style);
 
 export default function Table({
   items,
-}: {
-  items: TrackSearchResult['items'] | undefined;
-}) {
+}: Readonly<{
+  items: TrackSearchResult['items'];
+}>) {
   const { data } = useCurrentPlayingTrack();
   return (
     <ul className={style('table')}>
-      {items?.map(item => (
-        <li className={style('table-item')} key={item?.id}>
+      {items.map(item => (
+        <li className={style('table-item')} key={item.id}>
           <li>
-            <img src={item?.album?.images && item?.album?.images[0]?.url} />
+            <img
+              src={
+                item?.album?.images == null
+                  ? PLACEHOLDER_IMAGE
+                  : item?.album?.images[0]?.url
+              }
+              alt={`${item?.album.name} 앨범 커버 이미지`}
+            />
           </li>
 
           <li className={style('table-item-description')}>
