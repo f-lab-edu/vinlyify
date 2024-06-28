fa2eb101bbf0937a3e36860115be507352c5766d
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-28 20:00:03 +0900

    feat: scroll to top when page number,tab, search keyword urlParam change

---
+++ src/layout/ScrollToTop.tsx
@@ -0,0 +1,22 @@
+import useCurrentKeyword from '@/hooks/searchParams/useCurrentKeyword';
+import useCurrentPage from '@/hooks/searchParams/useCurrentPage';
+import useCurrentTab from '@/hooks/searchParams/useCurrentTab';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+  const { currentPage } = useCurrentPage();
+  const currentKeyword = useCurrentKeyword();
+  const currentTab = useCurrentTab();
+
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, [currentPage, currentKeyword, pathname, currentTab]);
+
+  return null;
+}
