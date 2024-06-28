ed51364c470564d423e75863d69ec327d48d4f3e
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-06-28 15:08:52 +0900

    feat: add ellipses for more than 4 multiprofile artists

--- src/components/Search/SearchResult/TabContent/_shared/MutliProfile/ProfileGroup.tsx
+++ src/components/Search/SearchResult/TabContent/_shared/MutliProfile/ProfileGroup.tsx
@@ -1,8 +1,23 @@
+import classNames from 'classnames/bind';
+import Style from './ellipses.module.scss';
 import Profile, { ProfileInfo } from './Profile';
 
+const style = classNames.bind(Style);
+
 const ProfileGroup = ({ profile }: { profile: ProfileInfo[] }) => {
-  return profile.map(v => {
-    return <Profile key={v.img} profile={v} />;
-  });
+  return (
+    <>
+      {profile.slice(0, 4).map(v => (
+        <Profile key={v.img} profile={v} />
+      ))}
+      {profile.length < 4 ? null : (
+        <>
+          <span className={style('ellipses', 'wrap')}>
+            <span>...</span>
+          </span>
+        </>
+      )}
+    </>
+  );
 };
 export default ProfileGroup;
