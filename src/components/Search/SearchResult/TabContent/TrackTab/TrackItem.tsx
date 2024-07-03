613e94dd31fb354d45b371f3a4f8b5787ae22e34
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-07-03 16:33:05 +0900

    feat: add InfiniteTrackList component

--- src/components/Search/SearchResult/TabContent/TrackTab/TrackItem.tsx
+++ src/components/Search/SearchResult/TabContent/TrackTab/TrackItem.tsx
@@ -2,19 +2,19 @@ import { PLACEHOLDER_IMAGE } from '@/constants/image';
 import { Track } from '@/models/Track';
 import { HHMMSSFormat } from '@/utils/time';
 import { useEffect, useMemo, useState } from 'react';
+import { TabProps } from '..';
 import Card from '../_shared/Card';
 import CoverImage from '../_shared/CoverImage';
 import MultiProfile from '../_shared/MutliProfile';
 import { useMultiProfileImg } from '../_shared/MutliProfile/hooks/useMultiProfileImg';
 import Profile from '../_shared/MutliProfile/Profile';
 
-const TrackItem = ({
-  item,
-  artistImgUrls,
-}: {
+interface TrackItemProps extends TabProps {
   item: Track;
   artistImgUrls: Map<string, string>;
-}) => {
+}
+
+const TrackItem = ({ item, artistImgUrls, innerRef }: TrackItemProps) => {
   const artistInfo = useMultiProfileImg({ item, artistImgUrls });
   const [validTrackArtistInfo, setValidTrackArtistInfo] = useState(() =>
     artistInfo?.every(item => item.img !== undefined),
@@ -33,6 +33,7 @@ const TrackItem = ({
     <Card
       title={item?.name}
       contextUri={item?.uri}
+      innerRef={innerRef}
       title_tag={`${item?.album?.name} #${item?.track_number}`}
       topContent={
         item?.album?.images ? (
