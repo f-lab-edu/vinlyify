468fa07d135a31fea72f878ce826f7b2c0c05a5d
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-07-09 17:26:25 +0900

    docs: add todo, remove comment after merge

--- src/components/Search/SearchResult/TabContent/AlbumTab/AlbumItem.tsx
+++ src/components/Search/SearchResult/TabContent/AlbumTab/AlbumItem.tsx
@@ -1,6 +1,14 @@
 import { PLACEHOLDER_IMAGE } from '@/constants/image';
 import { Album } from '@/models/Album';
 import { useEffect, useState } from 'react';
+/**
+ * !!TODO 모든 Tab 컴포넌트 PR 머지 후 SearchResult/TabContent/index.tsx에 변경 사항 반영되면 삭제하기
+ * 모든 탭 완성 후 탭 프롭스 인터페이스가
+ * export interface TabProps {
+ * innerRef?: (node?: Element | null) => void | null;
+ * }
+ * 사용처인 SearchResult/TabContent/index.tsx에 추가될 예정입니다.
+ */
 import { TabProps } from '..';
 import Card from '../_shared/Card';
 import CoverImage from '../_shared/CoverImage';
