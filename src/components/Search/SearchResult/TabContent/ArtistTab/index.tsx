c4c9f979377c23855899d86f672b451d2155d25e
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-07-09 17:37:33 +0900

    feat: add innerRef for last card item in artist tab

--- src/components/Search/SearchResult/TabContent/ArtistTab/index.tsx
+++ src/components/Search/SearchResult/TabContent/ArtistTab/index.tsx
@@ -1,17 +1,27 @@
-import { Loading } from '@/components/Main';
 import { Artist } from '@/models/Profile';
-import Grid from '../_shared/Grid';
+/**
+ * !!TODO 모든 Tab 컴포넌트 PR 머지 후 SearchResult/TabContent/index.tsx에 변경 사항 반영되면 삭제하기
+ * 모든 탭 완성 후 탭 프롭스 인터페이스가
+ * export interface TabProps {
+ * innerRef?: (node?: Element | null) => void | null;
+ * }
+ * 사용처인 SearchResult/TabContent/index.tsx에 추가될 예정입니다.
+ */
+import { TabProps } from '..';
 import ArtistItem from './ArtistItem';
 
-const ArtistTab = ({ tabItem }: { tabItem: Artist[] }) => {
-  if (tabItem?.length === 0) return <Loading />;
-  return (
-    <Grid>
-      {tabItem.map(item => (
-        <ArtistItem item={item} key={item.id} />
-      ))}
-    </Grid>
+interface ArtistListProps extends TabProps {
+  tabItem: Artist[];
+}
+
+const ArtistList = ({ tabItem, innerRef }: ArtistListProps) => {
+  return tabItem.map((item, index) =>
+    index === tabItem.length - 1 ? (
+      <ArtistItem item={item} key={item.id} innerRef={innerRef} />
+    ) : (
+      <ArtistItem item={item} key={item.id} />
+    ),
   );
 };
 
-export default ArtistTab;
+export default ArtistList;
