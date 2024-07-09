2287baa464fb88d1ae953db0766828969715d1ea
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-07-09 17:48:18 +0900

    docs: add todo

--- src/components/Search/SearchResult/TabContent/PlaylistTab/PlaylistItem.tsx
+++ src/components/Search/SearchResult/TabContent/PlaylistTab/PlaylistItem.tsx
@@ -3,7 +3,14 @@ import { Playlist } from '@/models/Playlist';
 import classNames from 'classnames/bind';
 import Card from '../_shared/Card';
 import CoverImage from '../_shared/CoverImage';
-
+/**
+ * !!TODO 모든 Tab 컴포넌트 PR 머지 후 SearchResult/TabContent/index.tsx에 변경 사항 반영되면 삭제하기
+ * 모든 탭 완성 후 탭 프롭스 인터페이스가
+ * export interface TabProps {
+ * innerRef?: (node?: Element | null) => void | null;
+ * }
+ * 사용처인 SearchResult/TabContent/index.tsx에 추가될 예정입니다.
+ */
 import { TabProps } from '..';
 import Style from './playlist-item.module.scss';
 
